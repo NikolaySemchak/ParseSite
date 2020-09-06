f1c136d1c5b7e0ef9796cc7a37117e509d62f4d4
Nikolay Semchak <kolpop.kolian@gmail.com>
2020-09-06 23:23:13 +0300

    Final

--- spam.py
+++ spam.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import os
+import time
 
 def main():
     driver = webdriver.Chrome(os.getcwd() + "//chromedriver")
@@ -9,18 +10,19 @@ def main():
     driver.find_element_by_xpath('//*[@id="pet-tab"]/ul/li[3]/a').click()
 
     theEnd = int(driver.find_element_by_xpath('//*[@id="pet-tab-3"]/div/div[3]/div/ul/li[7]/a').text) + 2
+
     i = 0
     f = open('peoples', 'a')
     while i < theEnd:
+        time.sleep(1)
         peoples = driver.find_elements_by_class_name("table_row")
         
         for name in peoples:
             f.write(name.text + '\n')
         
         allBtn = driver.find_elements_by_class_name('pag_link')
-        allBtn[len(allBtn) - 1].click()        
+        allBtn[len(allBtn) - 1].click()
         i += 1
-
     f.close()   
     
 
