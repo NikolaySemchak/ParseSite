6764c542763fd6d74025f810579044102cb482c4
Nikolay Semchak <kolpop.kolian@gmail.com>
2020-09-18 11:43:35 +0300

    last commit

--- spam.py
+++ spam.py
@@ -5,14 +5,14 @@ import time
 def main():
     driver = webdriver.Chrome(os.getcwd() + "//chromedriver")
 
-    driver.get("https://petition.president.gov.ua/petition/100754")
+    driver.get("https://petition.president.gov.ua/petition/103960")
 
     driver.find_element_by_xpath('//*[@id="pet-tab"]/ul/li[3]/a').click()
 
     theEnd = int(driver.find_element_by_xpath('//*[@id="pet-tab-3"]/div/div[3]/div/ul/li[7]/a').text) + 2
 
     i = 0
-    f = open('peoples', 'a')
+    f = open('peoples2', 'a')
     while i < theEnd:
         time.sleep(1)
         peoples = driver.find_elements_by_class_name("table_row")
